9aa33c8b1fd5e7e3518adcc53dc9e2e238dbb33d
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-20 18:40:57 +0400

    parent and child components communicating via props to each other

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,17 +1,39 @@
-export const Profile = () => {
-  const imgPath =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Portrait_Gandhi.jpg/250px-Portrait_Gandhi.jpg";
-  const altTxt = "Mahatma Gandhi";
-  return <img src={imgPath} alt={altTxt} />;
+export const Profile = (props) => {
+  const imgPath = props.imgPath;
+  const altTxt = props.altTxt;
+  return (
+    <img
+      src={imgPath}
+      alt={altTxt}
+      style={{ width: "300px", height: "400px" }}
+    />
+  );
 };
 
 const Gallery = () => {
+  const profiles = [
+    {
+      imgPath:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Portrait_Gandhi.jpg/250px-Portrait_Gandhi.jpg",
+      altTxt: "Mahatma Gandhi",
+    },
+    {
+      imgPath:
+        "https://m.media-amazon.com/images/M/MV5BNmZmOWVmOTAtZTE3OS00OTBlLTg3M2EtYWRlNDI0NzI5OWY3XkEyXkFqcGdeQXVyNDI3NjU1NzQ@._V1_.jpg",
+      altTxt: "Aishwarya Rai Bachchan",
+    },
+    {
+      imgPath:
+        "https://cdn.britannica.com/82/220482-050-17843B41/Priyanka-Chopra-2020.jpg",
+      altTxt: "Priyanka Chopra",
+    },
+  ];
   return (
     <section>
       <h1 style={{ color: "red", backgroundColor: "pink" }}>Amazing Indians</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Profile {...profiles[0]} />
+      <Profile imgPath={profiles[1].imgPath} altTxt={profiles[1].altTxt} />
+      <Profile imgPath={profiles[2].imgPath} altTxt={profiles[2].altTxt} />
     </section>
   );
 };

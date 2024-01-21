acacb1cc87af884a9c0e88577a5caaca648b1d38
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-21 16:52:29 +0400

    stop propagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-const Button = ({children,onSmash}) =>{
-  return (
-    <button onClick={onSmash}>
-      {children}
-    </button>
-  )
-}
-
+import Toolbar from "./Toolbar";
 
 function App() {
-  const smashHandler = ()=>{
-  console.log('Smashed');
-  }
  return <>
-  <Button onSmash={smashHandler}></Button>
+  <Toolbar/>
  </>
 }
 

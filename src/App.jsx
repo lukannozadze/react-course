f2701fae8c144e1564af9a05dd34d8c612d51bf0
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-23 15:06:42 +0400

    simple use of useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import Toolbar from "./Toolbar";
+import { useState } from "react";
 
 function App() {
- return <>
-  <Toolbar/>
- </>
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >Increment</button>
+      <h1>{count}</h1>
+    </>
+  );
 }
 
 export default App;

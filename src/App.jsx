96763824607e7572f541a748e9903a73d5cee54d
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-28 15:46:39 +0400

    initial commit of new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,8 @@
-import {useState} from "react"
-import { ContextProvider,useContextProvider } from "./providers/Provider";
-import Button from "./components/Button";
-
-function App() {
-  const [sumIsShown,setSumIsShown] = useState(false);
-  const [isReset,setIsReset] = useState(false);
-  const mainBtnValueObj = useContextProvider();
-
-
-  const buttons = Array.from({ length: 8 }).map((_, idx) => (
-    <Button isReset={isReset} setIsReset={setIsReset} setSumIsShown={setSumIsShown} setSum={mainBtnValueObj.setSum} key={idx} />
-  ));
-
-  const mainBtnClickHandler  = () =>{
-    setSumIsShown(true);
-  }
-
-  const resetClickHandler = () =>{
-    mainBtnValueObj.setSum(0);
-    setIsReset(true);
-  }
-  return (
-    <>
-      <div className="app-container">
-        <button className="btn-main" onClick={mainBtnClickHandler}>Main Button</button>
-        <button className="btn-reset"  onClick={resetClickHandler}>Reset</button>
-        {sumIsShown && <p className="score">{'SUM: ' + mainBtnValueObj.sum}</p>}
-        <div className="btn-container">{buttons}</div>
-      </div>
-    </>
-  );
+const App = () =>{
+return (
+  <>
+  <h1>React Intermediate</h1>
+  </>
+)
 }
-
-export default function WithProvider() {
-  return (
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  );
-}
+export default App;

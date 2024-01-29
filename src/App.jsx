5d499219e9380c2af7fe982bcc113ab26a3f6827
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-29 14:48:14 +0400

    make custom useMemo hook and realize its flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-const App = () =>{
-return (
-  <>
-  <h1>React Intermediate</h1>
-  </>
-)
-}
+const App = () => {
+  const addNums = (num1, num2) => {
+    return num1 + num2;
+  };
+  const x = useCustomMemo(addNums);
+  console.log(x(1,2));
+  console.log(x(1,2));
+  return (
+    <>
+      <h1>React Intermediate</h1>
+    </>
+  );
+};
 export default App;
+
+const useCustomMemo = (fn) => {
+  const cache = {};
+  
+  return (...args) => {
+    if (args.toString() in cache) {
+      console.log(cache);
+      return cache[args.toString()];
+    }
+    console.log("fire");
+    const result = fn(...args);
+    cache[args.toString()] = result;
+    return result;
+  };
+};

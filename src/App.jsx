c8992074e171a398ccd1f1f1af516670b059e51c
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-01-30 17:19:16 +0400

    useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import {useState,useMemo} from 'react'
+import {useState,useMemo, useCallback} from 'react'
 const App = () => {
   const [number,setNumber] = useState(0);
   const [clicked,setClicked] = useState(false);
-  const doubledNumber = useMemo(()=>{
-    return multiplied(number)
+  const doubledNumber = useCallback((coefficient)=>{
+    return multiplied(number,coefficient)
   },[number]);
+
   const style=useMemo(()=>{
+
     return {backgroundColor:clicked?'black':'white',
     color:clicked?'white':'black'}
   },[clicked])
+
   const inputChangeHandler = (event) =>{
      setNumber(event.target.value);
   }
@@ -17,14 +20,14 @@ const App = () => {
       setClicked(!clicked);
   }
 
-  function multiplied(num){
+  function multiplied(num,coefficient){
     for(let i=0; i<1000000000;i++){}
-    return num*2;
+    return num*coefficient;
   }
  return (<>
  <input onChange={inputChangeHandler} type="number"></input>
  <button onClick={buttonClickHandler}>Change Theme</button>
- <p style={style}>{doubledNumber}</p>
+ <p style={style}>{doubledNumber(5)}</p>
  </>)
 }
 export default App;

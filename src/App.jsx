495d079b71ffe47ea534270a1aac45615d2a80a2
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-05 15:48:04 +0400

    use render props pattern to pass component as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import  withStyles  from "./components/withStyles";
+import { useState } from "react";
 
 const App = () => {
-   const NewParagraph = withStyles(Paragraph);
-   const NewHeader = withStyles(Header);
-  return (<div>
-    <NewParagraph/>
-    <NewHeader/>
-  </div>);
+  return <TemperatureConverter/>
 };
 export default App;
 
-const Paragraph = ({styles}) =>{
-  return <p style={styles}>RAME</p>
-}
-const Header = ({styles}) =>{
- return <h1 style={styles}>Hadada</h1>
-}
+const TemperatureConverter = () => {
+  return <TemperatureForm
+    convertKelvin={(props) => <h2 key={1}>{props.value}</h2>}
+    convertFahrenheit={(props) => <h3 key={2}>{props.value}</h3>}
+    rectangle = {Rectangle}
+  />;
+};
+const Rectangle = (props)=>[
+  <div key={3} style={{width:'200px',height:'200px',backgroundColor:'orange'}}>{props.unique}</div>
+]
+const TemperatureForm = (props) => {
+  const [value, setValue] = useState("");
+  const unique = 50;
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        value={value}
+      />
+      {props.convertKelvin({ value: +value + 999 })}
+      {props.convertFahrenheit({ value: +value + 111 })}
+      {props.rectangle({unique:unique})}
+    </div>
+  );
+};
